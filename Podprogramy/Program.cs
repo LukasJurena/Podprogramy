97cfe1e243e33e53c731aa3652929e49d8d203a8
LukasJurena <jurenalukash@gmail.com>
2026-01-05 08:29:26 +0100

    Přidejte soubory projektu.

---
+++ Podprogramy/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Podprogramy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VypisTextu("Ahoj, tady vypisuji text přes proceduru.");
+
+            int a = 5;
+            int b = 10;
+            int soucet = SectiCisla(a, b);
+            Console.WriteLine($"Součet čísel {a} a {b} je {soucet}");
+
+            int vek = 20;
+            if (JePlnolety(vek))
+            {
+                Console.WriteLine($"Osoba, která má {vek} je plnoletá.");
+            }
+            else
+            {
+                Console.WriteLine($"Osoba, která má {vek} let není plnoletá.");
+            }
+            Console.ReadLine();
+        }
+        //procedura - nevrací hodnotu
+        static void VypisTextu(string text)
+        {
+            Console.WriteLine(text);
+        }
+        //funkce - vrací hodnotu
+        static int SectiCisla(int a, int b)
+        {
+            return a + b;
+        }
+        //funkce - vrací hodnotu
+        static bool JePlnolety(int vek)
+        {
+            if (vek >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Podprogramy/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("Podprogramy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Podprogramy")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("f3c2c5ff-8e27-4fe8-8f53-1631e03e47b4")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
